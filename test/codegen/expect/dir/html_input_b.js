a1a03ea1e22df63db775d65eb3a50291657a33d0
algobardo <mezzetti@gmail.com>
2015-10-27 11:31:20 +0100

    tests removed

--- test/codegen/expect/dir/html_input_b.js
+++
@@ -1,6 +0,0 @@
-dart.library('dir/html_input_b', null, /* Imports */[
-], /* Lazy imports */[
-], function(exports) {
-  'use strict';
-  exports.x = 3;
-});
--- test/codegen/expect/dir/html_input_e.js
+++
@@ -1,6 +0,0 @@
-dart.library('dir/html_input_e', null, /* Imports */[
-], /* Lazy imports */[
-], function(exports) {
-  'use strict';
-  exports.w = 9;
-});
